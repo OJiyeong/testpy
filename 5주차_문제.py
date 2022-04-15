9c6df45b6bdcdfa15501e6be50bc8533cdb0af91
OJiyeong <102336876+OJiyeong@users.noreply.github.com>
2022-04-15 10:22:02 +0900

    Update 5주차_문제.py

--- 5주차_문제.py
+++ 5주차_문제.py
@@ -1,2 +1,8 @@
 #words = 'When God Close One Door, He Opens Another.'
 #문자열 속 대문자는 몇 번째 인덱스에 있는지 모두 구하시오.
+
+print(words)
+print('대문자 인덱스 : ', end='')
+for i in words :
+    if i >= 'A' and i <= 'Z' :
+        print(words.index(i), end = ' ')
